27716f1cade9583d4e71abf4265011c2cad80ea0
Nat! <nat@mulle-kybernetik.com>
2016-07-23 17:27:08 +0200

    "outsource" test script code

--- tests/build-for-test.sh
+++ tests/build-for-test.sh
@@ -1,11 +1,1 @@
-#! /bin/sh
-
-set -e
-
-cd ..
-   mulle-bootstrap build  -c Debug -k "$@"
-
-cd build
-   cmake -DCMAKE_OSX_SYSROOT=macosx -DCMAKE_INSTALL_PREFIX="`pwd`/.." -DCMAKE_BUILD_TYPE=Debug ..
-   make install
-
+../mulle-tests/build-for-test.sh
