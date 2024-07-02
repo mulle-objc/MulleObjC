31bfe43740570ca6650291eb3d5a1cfda07e854c
Nat! <nat@mulle-kybernetik.com>
2024-07-02 12:58:04 +0200

    * `mulleGainAccess` no longer returns self * the Spam functions that used to do call chains, now use mulle_objc_class_search and perform more sensibly * new method `mulleSetThredSafe:`, for private objects that are part of a locking object (e.g. NSMutableArray ivar)

---
+++ test/MulleObject/20-misc/verify-unlocked2.m
@@ -0,0 +1,92 @@
+#import <MulleObjC/MulleObjC.h>
+
+#import <MulleObjC/MulleObjCDebug.h>
+
+
+//
+// here verify is declared as threadsafe, so the lock will not be used
+// when calling -verify. Ergo another thread should be able to lock
+// test:. But test: could be already locked ? No problem it is recursive!
+//
+@interface A : MulleObject < MulleAutolockingObjectProtocols>
+@end
+
+
+
+@implementation A
+
++ (void) test:(id) a
+{
+   if( [a tryLock])
+   {
+      mulle_printf( "OK\n");
+      [a unlock];
+   }
+   else
+      mulle_printf( "`a` could not be locked from a different thread. Dis bad!\n");
+}
+
+
+// some funny code to test an obscure problem
+- (void) release
+{
+   NSThread   *thread;
+
+   mulle_thread_once_do( test)
+   {
+      @autoreleasepool
+      {
+         thread = [[NSThread instantiate] initWithTarget:[A class]
+                                                selector:@selector( test:)
+                                                  object:self];
+         [thread mulleStartUndetached];
+         [thread mulleJoin];
+      }
+   }
+
+   [super release];
+}
+
+@end
+
+
+
+int  main( int argc, char *argv[])
+{
+   A   *a;
+
+   a = [A new];
+   [a release];
+
+   return( 0);
+}
+
+
+/*
+ * #### Advertisement ####
+ *
+ * Check for leaks with mulle-testallocator! Add mulle-testallocator to your
+ * project:
+ *
+ * mulle-sde dependency add --marks all-load,no-singlephase \
+ *                          --github mulle-core \
+ *                          mulle-testallocator
+ *
+ * Then build your project again and run your executable with the following
+ * environment variables:
+ *
+ *    MULLE_OBJC_PEDANTIC_EXIT=YES
+ *    MULLE_TESTALLOCATOR=YES
+ *
+ * To easier pin down, where a leak is created. try any of:
+ *
+ *    MULLE_TESTALLOCATOR_TRACE=3
+ *    MULLE_OBJC_TRACE_INSTANCE=YES
+ *    MULLE_OBJC_TRACE_METHOD_CALL=YES
+ *    MULLE_OBJC_TRACE_UNIVERSE=YES
+ *
+ * If you are writing singleton code try:
+ *
+ *    MULLE_OBJC_EPHEMERAL_SINGLETON=YES
+ *
+ */
