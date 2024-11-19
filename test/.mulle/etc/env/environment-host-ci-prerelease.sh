eb774c4f6fb91f6bfdcf77272de42086886c17e5
Your Name <you@example.com>
2024-11-19 15:33:10 +0100

    update various version files

--- test/.mulle/etc/env/environment-host-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-ci-prerelease.sh
@@ -35,6 +35,60 @@ export MULLE__OBJC__RUNTIME_BRANCH="prerelease"
 
 
 #
+# mulle-core nodetype: ${MULLE__CORE_NODETYPE:-tar}
+#
+export MULLE__CORE_NODETYPE="git"
+
+
+#
+# mulle-core url: ${MULLE__CORE_URL:-https://github.com/mulle-core/mulle-core/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__CORE_URL="https://github.com/mulle-core/mulle-core.git"
+
+
+#
+# mulle-core branch: ${MULLE__CORE_BRANCH}
+#
+export MULLE__CORE_BRANCH="prerelease"
+
+
+#
+# mulle-atinit nodetype: ${MULLE__ATINIT_NODETYPE:-tar}
+#
+export MULLE__ATINIT_NODETYPE="git"
+
+
+#
+# mulle-atinit url: ${MULLE__ATINIT_URL:-https://github.com/mulle-core/mulle-atinit/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__ATINIT_URL="https://github.com/mulle-core/mulle-atinit.git"
+
+
+#
+# mulle-atinit branch: ${MULLE__ATINIT_BRANCH}
+#
+export MULLE__ATINIT_BRANCH="prerelease"
+
+
+#
+# mulle-atexit nodetype: ${MULLE__ATEXIT_NODETYPE:-tar}
+#
+export MULLE__ATEXIT_NODETYPE="git"
+
+
+#
+# mulle-atexit url: ${MULLE__ATEXIT_URL:-https://github.com/mulle-core/mulle-atexit/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__ATEXIT_URL="https://github.com/mulle-core/mulle-atexit.git"
+
+
+#
+# mulle-atexit branch: ${MULLE__ATEXIT_BRANCH}
+#
+export MULLE__ATEXIT_BRANCH="prerelease"
+
+
+#
 # mulle-objc-debug nodetype: ${MULLE__OBJC__DEBUG_NODETYPE:-tar}
 #
 export MULLE__OBJC__DEBUG_NODETYPE="git"
@@ -43,7 +97,7 @@ export MULLE__OBJC__DEBUG_NODETYPE="git"
 #
 # mulle-objc-debug url: ${MULLE__OBJC__DEBUG_URL:-https://github.com/mulle-objc/mulle-objc-debug/archive/${MULLE_TAG}.tar.gz}
 #
-export MULLE__OBJC__DEBUG_URL="https://github.com/mulle-objc/mulle-objc-debug.tar.git"
+export MULLE__OBJC__DEBUG_URL="https://github.com/mulle-objc/mulle-objc-debug.git"
 
 
 #
@@ -107,42 +161,6 @@ export MULLE_OBJC__STARTUP_BRANCH="prerelease"
 
 
 #
-# mulle-atinit nodetype: ${MULLE__ATINIT_NODETYPE:-tar}
-#
-export MULLE__ATINIT_NODETYPE="git"
-
-
-#
-# mulle-atinit url: ${MULLE__ATINIT_URL:-https://github.com/mulle-core/mulle-atinit/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ATINIT_URL="https://github.com/mulle-core/mulle-atinit.git"
-
-
-#
-# mulle-atinit branch: ${MULLE__ATINIT_BRANCH}
-#
-export MULLE__ATINIT_BRANCH="prerelease"
-
-
-#
-# mulle-atexit nodetype: ${MULLE__ATEXIT_NODETYPE:-tar}
-#
-export MULLE__ATEXIT_NODETYPE="git"
-
-
-#
-# mulle-atexit url: ${MULLE__ATEXIT_URL:-https://github.com/mulle-core/mulle-atexit/archive/${MULLE_TAG}.tar.gz}
-#
-export MULLE__ATEXIT_URL="https://github.com/mulle-core/mulle-atexit.git"
-
-
-#
-# mulle-atexit branch: ${MULLE__ATEXIT_BRANCH}
-#
-export MULLE__ATEXIT_BRANCH="prerelease"
-
-
-#
 # mulle-testallocator nodetype: ${MULLE__TESTALLOCATOR_NODETYPE:-tar}
 #
 export MULLE__TESTALLOCATOR_NODETYPE="git"
@@ -176,6 +194,24 @@ export MULLE__THREAD_URL="https://github.com/mulle-concurrent/mulle-thread.git"
 # mulle-thread branch: ${MULLE__THREAD_BRANCH}
 #
 export MULLE__THREAD_BRANCH="prerelease"
+
+
+#
+# mulle-c11 nodetype: ${MULLE__C11_NODETYPE:-tar}
+#
+export MULLE__C11_NODETYPE="git"
+
+
+#
+# mulle-c11 url: ${MULLE__C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE__C11_URL="https://github.com/mulle-c/mulle-c11.git"
+
+
+#
+# mulle-c11 branch: ${MULLE__C11_BRANCH}
+#
+export MULLE__C11_BRANCH="prerelease"
 
 
 #
@@ -230,5 +266,23 @@ export MULLE__DLFCN_URL="https://github.com/mulle-core/mulle-dlfcn.git"
 # mulle-dlfcn branch: ${MULLE__DLFCN_BRANCH}
 #
 export MULLE__DLFCN_BRANCH="prerelease"
+
+
+#
+# dlfcn-win32 nodetype: ${DLFCN__WIN32_NODETYPE:-tar}
+#
+export DLFCN__WIN32_NODETYPE="git"
+
+
+#
+# dlfcn-win32 url: ${DLFCN__WIN32_URL:-https://github.com/mulle-core/dlfcn-win32/archive/refs/tags/${MULLE_TAG}.tar.gz}
+#
+export DLFCN__WIN32_URL="https://github.com/mulle-core/dlfcn-win32.git"
+
+
+#
+# dlfcn-win32 branch: ${DLFCN__WIN32_BRANCH}
+#
+export DLFCN__WIN32_BRANCH="prerelease"
 
 
